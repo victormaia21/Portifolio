f6c83425fc88762e61279521bea86ec6a119f126
Victor <jv1555550@gmail.com>
2024-04-22 23:18:53 -0500

    .

---
+++ src/components/CardProjeto/Index.tsx
@@ -0,0 +1,32 @@
+import "../../styles/index.scss";
+
+interface Props {
+    img: string;
+    description: string;
+    title: string;
+    link: string;
+    setIsOpen:(open : boolean) => void;
+    isOpen:boolean;
+}
+
+function Index({ img, description, title, link, setIsOpen, isOpen } : Props) {
+  return (
+    <div className={isOpen ? "card card_open" : "card"}>
+        <div className="card_img">
+            <img src={img} alt="logo" />
+        </div>
+        <p className="title_card">{title}</p>
+        <p className="description_card">
+            {description}
+        </p>
+        <div className="buttons_card">
+            <button>
+                <a href={link} target="_blank">Detalhes</a>
+            </button>
+            <button onClick={() => setIsOpen(false)}>Fechar</button>
+        </div>
+    </div>
+  )
+}
+
+export default Index
--- src/components/Projetos/Index.tsx
+++ src/components/Projetos/Index.tsx
@@ -1,12 +1,83 @@
 import "../../styles/index.scss";
+import Decorp from '../../assets/images/decorp.png';
+import Itroca from '../../assets/images/itroca.png';
+import { useState } from "react";
+import CardProjeto from '../CardProjeto/Index';
 
 function Index() {
+
+  interface IcardData {
+    title: string;
+    description: string;
+    img:string;
+    link:string;
+  }
+
+  const [cardOpen, setCardOpen] = useState<boolean>(false);
+  const [cardData, setCardData] = useState<IcardData>({
+    description:"",
+    title:"",
+    img:"",
+    link:""
+  });
+
+  const cardDataOpen = (description : string, title:string, img:string, link:string) => {
+    setCardData({
+      description,
+      title,
+      img,
+      link
+    });
+    setCardOpen(true);
+  }
+
   return (
-    <div className="portifolio">
+    <>
+    <div className={cardOpen ? "page" : ""}></div>
+      <div className="portifolio">
         <h2>PORTIFOLIO</h2>
         <h1>Confira alguns projetos realizado</h1>
-        <h1>Nenhum projeto feito ainda</h1>
+        <div className="projects">
+            <div 
+              onClick={
+                () => cardDataOpen(
+                  `Desenvolvido utilizando Node.js e React, este projeto demandou aproximadamente 7 meses para ser 
+                  concluído. Seu objetivo é facilitar a contratação de profissionais de diferentes áreas para a 
+                  prestação de serviços específicos.`,
+                  "DECORP",
+                  Decorp,
+                  "http://34.206.239.17:5173/"
+                )
+              }
+            >
+              <img src={Decorp} alt="decorp" />
+              {cardOpen ? null : <h1>DECORP</h1>}
+            </div>
+            <div
+              onClick={
+                () => cardDataOpen(
+                  `Projeto no qual participei criando a parte do front, itroca é um site para facilitar a 
+                  troca de iphones`,
+                  "ITROCA",
+                  Itroca,
+                  "https://www.itroca.com.br/"
+                )
+              }
+            >
+              <img src={Itroca} alt="itroca" />
+              {cardOpen ? null :<h1>ITROCA</h1>}
+            </div>
+        </div>
+          <CardProjeto
+            description={cardData.description}
+            img={cardData.img}
+            link={cardData.link}
+            title={cardData.title}
+            setIsOpen={setCardOpen}
+            isOpen={cardOpen}
+          />
     </div>
+    </>
   )
 }
 
